be26de504f8d20f7adf4df811693013ae1c003e2
MD HABIBOR RAHMAN HIRA <mdhera891@gmail.com>
2023-12-22 00:04:45 -0500

    Add useEffect hook to fetch IP address

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,14 +1,33 @@
 'use client'
 import VerticalTab from "@/components/VerticalTab";
+import { useEffect } from "react";
 
 const page = () => {
+
+
+
+  
+  
+  
+  useEffect(() => {
+    fetch('https://checkip.amazonaws.com/')
+    .then(res => res.text())
+    .then(json => console.log(json))
+  
+
+  }, []);
+
+
+
+
+
   return (
     <>
-    
-      <VerticalTab/>
 
-     
-    
+      <VerticalTab />
+
+
+
     </>
   );
 }
