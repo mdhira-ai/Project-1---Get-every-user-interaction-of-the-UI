f50bcceb769ff51a249e4898492942c1cf91fe89
MD HABIBOR RAHMAN HIRA <mdhera891@gmail.com>
2023-12-28 15:24:49 -0500

    Update socket URL in mysocket.js

--- my-app/lib/mysocket.js
+++ my-app/lib/mysocket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3002';
-export const socket = io(URL);
+// const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3002';
+export const socket = io('https://nodejs-production-8c25.up.railway.app/');
