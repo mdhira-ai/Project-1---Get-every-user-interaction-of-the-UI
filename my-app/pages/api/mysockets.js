833c6d7ce6b1f986b649a9f711aa62f6543f1f9c
MD HABIBOR RAHMAN HIRA <mdhera891@gmail.com>
2023-12-28 23:35:20 -0500

    Refactor socket handling in API endpoint

--- my-app/pages/api/mysockets.js
+++ my-app/pages/api/mysockets.js
@@ -1,43 +1,37 @@
 import { supabase } from "@/lib/supabase";
 import { Server } from "socket.io";
-import cors from "cors";
-const corsHandler = cors({
-    methods: ["GET", "POST", "HEAD"],
-    origin: "*",
-
-});
 
 export default function SocketHandler(req, res) {
-    corsHandler(req, res, () => {
-        const io = new Server(res.socket.server);
-        res.socket.server.io = io;
 
-        io.on("connection", (socket) => {
-            console.log("Socket connected: ", socket.id);
-            socket.on("send-message", (obj) => {
-                io.emit("receive-message", obj);
-            });
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
 
-            socket.on('disconnect', () => {
-                // for last modified
-                const date = new Date();
-                const lasttime = date.toLocaleTimeString();
-                console.log('User disconnected', socket.id);
-                // update disconnect user
-                let id = socket.id;
+    io.on("connection", (socket) => {
 
-                supabase.from('userinfo').update({
-                    lastModified: lasttime,
-                    Online: false
-                }).eq(
-                    "socketid", id
-                ).select().then((res) => {
-                    console.log(res.statusText);
-                });
-            });
+        console.log("Socket connected: ", socket.id);
+        socket.on("send-message", (obj) => {
+            io.emit("receive-message", obj);
         });
 
-        console.log("Setting up socket");
-        res.end();
+        socket.on('disconnect', () => {
+            // for last modified
+            const date = new Date();
+            const lasttime = date.toLocaleTimeString();
+            console.log('User disconnected', socket.id);
+            // update disconnect user
+            let id = socket.id;
+    
+            supabase.from('userinfo').update({
+                lastModified: lasttime,
+                Online: false
+            }).eq(
+                "socketid", id
+            ).select().then((res) => {
+                console.log(res.statusText);
+            });
+        });
     });
+
+    console.log("Setting up socket");
+    res.end();
 }
